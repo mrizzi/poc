5de82ee11751b427a0cab2071f64f886f7e1cbdb
mrizzi <mrizzi@redhat.com>
2021-02-03 20:11:32 +0100

    Minikube usage semplified with podman

---
+++ controls/src/main/resources/scripts/push-image-to-minikube.sh
@@ -0,0 +1,13 @@
+IMAGE_ID=$(podman images --filter=reference='localhost/'$USER'/'$1':'$2'-'$3 -n --format {{.ID}})
+TAR_NAME=$1'_'$IMAGE_ID'_'$(date +%Y%m%d%H%M%S)'.tar'
+TAR_PATH=/tmp/$TAR_NAME
+
+podman save -o $TAR_PATH $IMAGE_ID
+echo 'Saved temporary image '$IMAGE_ID' to file '$TAR_PATH
+eval $(minikube -p minikube podman-env)
+echo 'Evaluated minikube environment variables'
+podman-remote load -i $TAR_PATH $USER'/'$1':'$2'-'$3
+echo 'Loaded into minikube the image '$USER'/'$1':'$2'-'$3
+rm $TAR_PATH
+echo 'Removed file '$TAR_PATH
+kubectl rollout restart deployment controls
