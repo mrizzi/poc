8b85f9820b650a577aa118b78b4f10d778d5e37b
mrizzi <mrizzi@redhat.com>
2021-02-22 11:42:52 +0100

    Switched QEndpointsTest to check the 'health' endpoint that affects probes

--- controls/src/test/java/io/tackle/controls/resources/QEndpointsTest.java
+++ controls/src/test/java/io/tackle/controls/resources/QEndpointsTest.java
@@ -9,7 +9,7 @@ import static io.restassured.RestAssured.given;
 
 /**
  * Test done to be sure the `/q/*` endpoints (e.g. metrics, health, etc)
- * are not secured and so do not require any authentication
+ * are not secured and so do not require any authentication/authorization
  */
 @QuarkusTest
 // starting Keycloak to be sure it doesn't block request
@@ -20,7 +20,7 @@ public class QEndpointsTest extends SecuredResourceTest {
     public void testQEndpoints() {
         given().auth().oauth2("")
                 .accept("application/json")
-                .when().get("/q/metrics/")
+                .when().get("/q/health/")
                 .then()
                 .statusCode(200);
     }
