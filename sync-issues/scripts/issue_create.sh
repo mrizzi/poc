abfee9a9a18cf6cb3450540879b29dbe6d496574
mrizzi <mrizzi@redhat.com>
2021-02-25 01:17:11 +0100

    Changed Authorization

--- sync-issues/scripts/issue_create.sh
+++ sync-issues/scripts/issue_create.sh
@@ -7,4 +7,4 @@ echo $JIRA_ISSUE_ID
 echo $TITLE
 PAYLOAD="{ \"fields\": { \"project\": { \"key\": \"TACKLE\" }, \"parent\": { \"key\": \"$JIRA_ISSUE_ID\" }, \"summary\": \"[tackle-controls#"$NUMBER"]$TITLE\", \"description\": \"$DESCRIPTION\", \"issuetype\": { \"id\": 5 } } }"
 echo $PAYLOAD
-curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer "$BEARER -d "$PAYLOAD" https://issues.redhat.com/rest/api/2/issue/
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Basic "$BEARER -d "$PAYLOAD" https://issues.redhat.com/rest/api/2/issue/
