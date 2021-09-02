10bf909ec4b1a7d9203b2c60fb81016882bc36e8
mrizzi <mrizzi@redhat.com>
2021-09-02 18:24:49 +0200

    REST Graph Initial commit

---
+++ rest-graph/src/test/java/io/mrizzi/rest/NativeAnalysisResourceIT.java
@@ -0,0 +1,9 @@
+package io.mrizzi.rest;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeAnalysisResourceIT extends AnalysisResourceTest {
+
+    // Execute the same tests but in native mode.
+}
