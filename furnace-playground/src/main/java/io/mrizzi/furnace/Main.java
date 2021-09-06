75918fe74d603c587ecfb9f54a44858e8ef0bffc
mrizzi <mrizzi@redhat.com>
2021-09-06 19:05:53 +0200

    Adding some log to better understand how it works the addon registry

--- furnace-playground/src/main/java/io/mrizzi/furnace/Main.java
+++ furnace-playground/src/main/java/io/mrizzi/furnace/Main.java
@@ -1,16 +1,9 @@
 package io.mrizzi.furnace;
 
-import java.io.File;
-
-import java.nio.file.Files;
-import java.util.Collections;
-import java.util.concurrent.Future;
-
 import org.jboss.forge.addon.maven.projects.MavenBuildSystem;
 import org.jboss.forge.addon.parser.java.projects.JavaWebProjectType;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.ProjectFactory;
-import org.jboss.forge.addon.projects.ProjectProvider;
 import org.jboss.forge.addon.projects.facets.MetadataFacet;
 import org.jboss.forge.addon.resource.Resource;
 import org.jboss.forge.addon.resource.ResourceFactory;
@@ -18,15 +11,19 @@ import org.jboss.forge.addon.templates.Template;
 import org.jboss.forge.addon.templates.TemplateFactory;
 import org.jboss.forge.addon.templates.freemarker.FreemarkerTemplate;
 import org.jboss.forge.furnace.Furnace;
+import org.jboss.forge.furnace.addons.Addon;
 import org.jboss.forge.furnace.addons.AddonId;
 import org.jboss.forge.furnace.addons.AddonRegistry;
-import org.jboss.forge.furnace.manager.AddonManager;
-import org.jboss.forge.furnace.manager.impl.AddonManagerImpl;
-import org.jboss.forge.furnace.manager.maven.addon.MavenAddonDependencyResolver;
-import org.jboss.forge.furnace.manager.request.InstallRequest;
 import org.jboss.forge.furnace.repositories.AddonRepositoryMode;
 import org.jboss.forge.furnace.se.FurnaceFactory;
+import org.jboss.forge.furnace.spi.ServiceRegistry;
+import org.jboss.forge.furnace.util.Addons;
 import org.jboss.forge.furnace.util.OperatingSystemUtils;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.concurrent.Future;
 
 /**
  * This is an example on how to start Furnace in a standalone app
@@ -40,8 +37,36 @@ public class Main
         Furnace furnace = startFurnace();
         try
         {
+            System.out.println("furnace.getStatus() " + furnace.getStatus());
+            System.out.println("furnace.getRuntimeClassLoader() " + furnace.getRuntimeClassLoader());
             AddonRegistry addonRegistry = furnace.getAddonRegistry();
-            System.out.println(addonRegistry.getVersion());
+            System.out.println("addonRegistry.getVersion() " + addonRegistry.getVersion());
+            for (Addon addon : addonRegistry.getAddons()) {
+                ServiceRegistry serviceRegistry = addon.getServiceRegistry();
+                System.out.printf("Addon %s (%s) is started? %s\n", addon.getId(), serviceRegistry.getExportedInstance(ProjectFactory.class), addon.getStatus());
+            }
+/*
+            for (Class<?> clazz : addonRegistry.getExportedTypes()) {
+                System.out.printf("ExportedTypes %s\n", clazz.getName());
+            }
+*/
+/*
+            AddonId projects = AddonId.from("org.jboss.forge.addon:projects", "3.9.8.Final");
+            Addons.waitUntilStarted(addonRegistry.getAddon(projects));
+*/
+            ProjectFactory projectFactory = null;
+            while (projectFactory == null) {
+                try {
+                    Thread.sleep(1000);
+                    projectFactory = addonRegistry.getServices(ProjectFactory.class).get();
+                } catch (Exception e) {
+                    System.out.printf("Exception %s\n", e.getMessage());
+                }
+            }
+            createProject(addonRegistry);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
         finally
         {
@@ -55,7 +80,7 @@ public class Main
         Furnace furnace = FurnaceFactory.getInstance();
 
         // Add repository containing addons specified in pom.xml
-        furnace.addRepository(AddonRepositoryMode.IMMUTABLE, new File("furnace-playground/target/addons"));
+        furnace.addRepository(AddonRepositoryMode.IMMUTABLE, new File("target/addons"));
 
         // Start Furnace in another thread
         Future<Furnace> future = furnace.startAsync();
@@ -66,6 +91,7 @@ public class Main
 
     private static void createProject(AddonRegistry addonRegistry)
     {
+        System.out.println("createProject - addonRegistry.getVersion() " + addonRegistry.getVersion());
         ProjectFactory projectFactory = addonRegistry.getServices(ProjectFactory.class).get();
         ResourceFactory resourceFactory = addonRegistry.getServices(ResourceFactory.class).get();
 
@@ -93,6 +119,10 @@ public class Main
 
     private static void createTemplate(AddonRegistry addonRegistry) throws Exception
     {
+        for (Addon addon : addonRegistry.getAddons()) {
+            ServiceRegistry serviceRegistry = addon.getServiceRegistry();
+            System.out.printf("createTemplate - Addon %s (%s) is started? %s\n", addon.getId(), serviceRegistry.getExportedInstance(ResourceFactory.class), addon.getStatus());
+        }
         ResourceFactory resourceFactory = addonRegistry.getServices(ResourceFactory.class).get();
         TemplateFactory templateFactory = addonRegistry.getServices(TemplateFactory.class).get();
         File tmpFile = File.createTempFile("template", ".tmp");
