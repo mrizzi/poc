1eb9413c88ab651c6a14617c9b0f0cf4921139a8
mrizzi <mrizzi@redhat.com>
2021-09-28 18:52:53 +0200

    Added index creation for 'winduptype' property

--- rest-graph/src/main/java/io/mrizzi/graph/GraphService.java
+++ rest-graph/src/main/java/io/mrizzi/graph/GraphService.java
@@ -3,6 +3,7 @@ package io.mrizzi.graph;
 import io.mrizzi.rest.WindupResource;
 import io.quarkus.runtime.Startup;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.janusgraph.core.Cardinality;
 import org.janusgraph.core.JanusGraph;
@@ -11,6 +12,7 @@ import org.janusgraph.core.PropertyKey;
 import org.janusgraph.core.schema.JanusGraphManagement;
 import org.janusgraph.util.system.ConfigurationUtil;
 import org.jboss.logging.Logger;
+import org.jboss.windup.graph.model.WindupEdgeFrame;
 import org.jboss.windup.graph.model.WindupVertexFrame;
 
 import javax.annotation.PostConstruct;
@@ -45,8 +47,10 @@ public class GraphService {
         final JanusGraph janusGraph = JanusGraphFactory.open(ConfigurationUtil.loadPropertiesConfig(centralGraphProperties));
         final JanusGraphManagement janusGraphManagement = janusGraph.openManagement();
         if (!janusGraphManagement.containsPropertyKey(WindupVertexFrame.TYPE_PROP)) {
-            final PropertyKey typePropPropertyKey = janusGraphManagement.makePropertyKey(WindupVertexFrame.TYPE_PROP).dataType(String.class).cardinality(Cardinality.LIST).make();
-            // TODO create the index for typePropPropertyKey
+            final PropertyKey vertexTypePropPropertyKey = janusGraphManagement.makePropertyKey(WindupVertexFrame.TYPE_PROP).dataType(String.class).cardinality(Cardinality.LIST).make();
+            janusGraphManagement.buildIndex("", Vertex.class).addKey(vertexTypePropPropertyKey).buildCompositeIndex();
+            final PropertyKey edgeTypePropPropertyKey = janusGraphManagement.makePropertyKey(WindupEdgeFrame.TYPE_PROP).dataType(String.class).cardinality(Cardinality.LIST).make();
+            janusGraphManagement.buildIndex("edge-typevalue", Vertex.class).addKey(edgeTypePropPropertyKey).buildCompositeIndex();
         }
         janusGraphManagement.commit();
         return janusGraph;
