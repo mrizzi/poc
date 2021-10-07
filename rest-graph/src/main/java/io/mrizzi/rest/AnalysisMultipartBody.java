194d437861268b9b950de04b4bb592ddc718ebcb
mrizzi <mrizzi@redhat.com>
2021-10-07 19:03:56 +0200

    POST '/analysis' endpoint: AnalysisMultipartBody

---
+++ rest-graph/src/main/java/io/mrizzi/rest/AnalysisMultipartBody.java
@@ -0,0 +1,17 @@
+package io.mrizzi.rest;
+
+import org.jboss.resteasy.annotations.providers.multipart.PartType;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.core.MediaType;
+import java.io.InputStream;
+
+public class AnalysisMultipartBody {
+    @FormParam("application") @PartType(MediaType.APPLICATION_OCTET_STREAM) InputStream applicationFile;
+    @FormParam("applicationFileName") @PartType(MediaType.TEXT_PLAIN) public String applicationFileName;
+    @FormParam("sources") @PartType(MediaType.TEXT_PLAIN) @DefaultValue("") String sources;
+    @FormParam("targets") @PartType(MediaType.TEXT_PLAIN) String targets;
+    @FormParam("packages") @PartType(MediaType.TEXT_PLAIN) @DefaultValue("")String packages;
+    @FormParam("sourceMode") @PartType(MediaType.TEXT_PLAIN) @DefaultValue("false") Boolean sourceMode;
+}
