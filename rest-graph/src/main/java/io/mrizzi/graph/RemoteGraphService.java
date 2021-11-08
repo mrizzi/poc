4710f9c094a090ea5112ed3def1515f245a524a0
mrizzi <mrizzi@redhat.com>
2021-11-08 14:54:30 +0100

    Created RemoteGraphService for managaing remote central graph

---
+++ rest-graph/src/main/java/io/mrizzi/graph/RemoteGraphService.java
@@ -0,0 +1,167 @@
+package io.mrizzi.graph;
+
+import com.syncleus.ferma.DelegatingFramedGraph;
+import com.syncleus.ferma.FramedGraph;
+import com.syncleus.ferma.ReflectionCache;
+import com.syncleus.ferma.framefactories.annotation.MethodHandler;
+import com.syncleus.ferma.typeresolvers.PolymorphicTypeResolver;
+import io.quarkus.runtime.Startup;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.Property;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.VertexProperty;
+import org.janusgraph.core.JanusGraph;
+import org.janusgraph.core.JanusGraphFactory;
+import org.janusgraph.util.system.ConfigurationUtil;
+import org.jboss.logging.Logger;
+import org.jboss.windup.graph.MapInAdjacentPropertiesHandler;
+import org.jboss.windup.graph.MapInAdjacentVerticesHandler;
+import org.jboss.windup.graph.MapInPropertiesHandler;
+import org.jboss.windup.graph.SetInPropertiesHandler;
+import org.jboss.windup.graph.WindupAdjacencyMethodHandler;
+import org.jboss.windup.graph.WindupPropertyMethodHandler;
+import org.jboss.windup.graph.javahandler.JavaHandlerHandler;
+import org.jboss.windup.graph.model.WindupEdgeFrame;
+import org.jboss.windup.graph.model.WindupFrame;
+import org.jboss.windup.graph.model.WindupVertexFrame;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import static io.mrizzi.rest.WindupResource.PATH_PARAM_APPLICATION_ID;
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
+
+@Startup
+@ApplicationScoped
+public class RemoteGraphService {
+    private static final Logger LOG = Logger.getLogger(RemoteGraphService.class);
+
+    private JanusGraph janusGraph;
+    private GraphTraversalSource g;
+
+    @PostConstruct
+    void init() throws Exception {
+        g = traversal().withRemote("conf/remote-graph.properties");
+    }
+
+    @PreDestroy
+    void destroy() throws Exception {
+        LOG.infof("Is central Janus Graph transaction open? %b", g.tx().isOpen());
+        g.close();
+        LOG.infof("Is central Janus Graph transaction still open? %b", g.tx().isOpen());
+    }
+
+    public void updateCentralJanusGraph(String sourceGraph, String applicationId) {
+        final ReflectionCache reflections = new ReflectionCache();
+        final AnnotationFrameFactory frameFactory = new AnnotationFrameFactory(reflections, getMethodHandlers());
+        final Map<Object, Object> verticesBeforeAndAfter = new HashMap<>();
+        try (JanusGraph janusGraph = openJanusGraph(sourceGraph);
+             FramedGraph framedGraph = new DelegatingFramedGraph<>(janusGraph, frameFactory, new PolymorphicTypeResolver(reflections))) {
+            // Delete the previous graph for the PATH_PARAM_APPLICATION_ID provided
+            LOG.infof("Delete the previous vertices with Application ID %s", applicationId);
+            if (LOG.isDebugEnabled())
+                LOG.debugf("Before deleting vertices with Application ID %s, central graph has %d vertices and %d edges", applicationId, g.V().count().next(), g.E().count().next());
+            final GraphTraversal<Vertex, Vertex> previousVertexGraph = g.V();
+            previousVertexGraph.has(PATH_PARAM_APPLICATION_ID, applicationId);
+            previousVertexGraph.drop().iterate();
+            if (LOG.isDebugEnabled())
+                LOG.debugf("After deletion of vertices with Application ID %s, central graph has %d vertices and %d edges", applicationId, g.V().count().next(), g.E().count().next());
+
+            final Iterator<WindupVertexFrame> vertexIterator = framedGraph.traverse(g -> g.V().has(WindupFrame.TYPE_PROP)).frame(WindupVertexFrame.class);
+            while (vertexIterator.hasNext()) {
+                WindupVertexFrame vertex = vertexIterator.next();
+                LOG.debugf("Adding Vertex %s", vertex);
+                Vertex importedVertex = g.getGraph().addVertex();
+                verticesBeforeAndAfter.put(vertex.getElement().id(), importedVertex.id());
+                Iterator<VertexProperty<String>> types = vertex.getElement().properties(WindupFrame.TYPE_PROP);
+                types.forEachRemaining(type -> type.ifPresent(value -> importedVertex.property(WindupFrame.TYPE_PROP, value)));
+                vertex.getElement().keys()
+                        .stream()
+                        .filter(s -> !WindupFrame.TYPE_PROP.equals(s))
+                        .forEach(property -> {
+                            LOG.debugf("Vertex %d has property %s with values %s", vertex.getElement().id(), property, vertex.getProperty(/*).getElement().properties(*/property));
+                            importedVertex.property(property, vertex.getProperty(/*).getElement().properties(*/property));
+//                    importedVertex.setProperty(property, vertex.getProperty(/*).getElement().properties(*/property));
+                        });
+                importedVertex.property(PATH_PARAM_APPLICATION_ID, applicationId);
+            }
+            if (LOG.isDebugEnabled())
+                LOG.debugf("Central Graph count after %d", g.V().count().next());
+//            g.V().toList().forEach(v -> LOG.infof("%s with property %s", v, v.property(PATH_PARAM_APPLICATION_ID)));
+            Iterator<WindupEdgeFrame> edgeIterator = framedGraph.traverse(GraphTraversalSource::E).frame(WindupEdgeFrame.class);
+            while (edgeIterator.hasNext()) {
+                WindupEdgeFrame edgeFrame = edgeIterator.next();
+                LOG.debugf("Adding Edge %s", edgeFrame.toPrettyString());
+                Edge edge = edgeFrame.getElement();
+
+                Object outVertexId = edge.outVertex().id();
+                Object importedOutVertexId = verticesBeforeAndAfter.get(outVertexId);
+                if (outVertexId == null || importedOutVertexId == null)
+                    LOG.warnf("outVertexId %s -> importedOutVertexId %s", outVertexId, importedOutVertexId);
+                Vertex outVertex = g.V(importedOutVertexId).next();
+
+                Object inVertexId = edge.inVertex().id();
+                Object importedInVertexId = verticesBeforeAndAfter.get(inVertexId);
+                if (inVertexId == null || importedInVertexId == null)
+                    LOG.warnf("inVertexId %s -> importedInVertexId %s", inVertexId, importedInVertexId);
+                GraphTraversal<Vertex, Vertex> edgeGraphTraversal = g.V(importedInVertexId);
+                Vertex inVertex = null;
+                if (edgeGraphTraversal.hasNext()) {
+                    inVertex = edgeGraphTraversal.next();
+                } else {
+                    LOG.warnf("Missing IN vertex. It seems like the %s vertex has not been imported", inVertexId);
+                    continue;
+                }
+                Edge importedEdge = outVertex.addEdge(edge.label(), inVertex/*, edge.properties()*/);
+                LOG.debugf("Added Edge %s", importedEdge);
+
+                Iterator<Property<String>> types = edge.properties(WindupEdgeFrame.TYPE_PROP);
+                types.forEachRemaining(type -> type.ifPresent(value -> importedEdge.property(WindupFrame.TYPE_PROP, value)));
+                edge.keys()
+                        .stream()
+                        .filter(s -> !WindupEdgeFrame.TYPE_PROP.equals(s))
+                        .forEach(property -> {
+                            LOG.debugf("Edge %d has property %s with values %s", edge.id(), property, edgeFrame.getProperty(property));
+                            importedEdge.property(property, edgeFrame.getProperty(property));
+                        });
+                importedEdge.property(PATH_PARAM_APPLICATION_ID, applicationId);
+            }
+            g.getGraph().tx().commit();
+        } catch (Exception e) {
+            LOG.errorf("Exception occurred: %s", e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Set<MethodHandler> getMethodHandlers() {
+        final Set<MethodHandler> handlers = new HashSet<>();
+        handlers.add(new MapInPropertiesHandler());
+        handlers.add(new MapInAdjacentPropertiesHandler());
+        handlers.add(new MapInAdjacentVerticesHandler());
+        handlers.add(new SetInPropertiesHandler());
+        handlers.add(new JavaHandlerHandler());
+        handlers.add(new WindupPropertyMethodHandler());
+        handlers.add(new WindupAdjacencyMethodHandler());
+        return handlers;
+    }
+
+    private JanusGraph openJanusGraph(String sourceGraph) throws ConfigurationException {
+        // temporary workaround to work locally
+        sourceGraph += "/graph/TitanConfiguration.properties";
+        LOG.infof("Opening Janus Graph properties file %s", sourceGraph);
+        PropertiesConfiguration configuration = ConfigurationUtil.loadPropertiesConfig(sourceGraph);
+        configuration.setProperty("storage.transactions", true);
+        return JanusGraphFactory.open(configuration);
+    }
+}
