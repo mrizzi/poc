d6d54d10644892dc6161b94fcf300453753d05f4
mrizzi <mrizzi@redhat.com>
2021-12-23 12:34:56 +0100

    Qute - Item sample from guide

---
+++ qute/src/main/java/io/mrizzi/Item.java
@@ -0,0 +1,8 @@
+package io.mrizzi;
+
+import java.math.BigDecimal;
+
+public class Item {
+    public String name;
+    public BigDecimal price;
+}
---
+++ qute/src/main/java/io/mrizzi/ItemResource.java
@@ -0,0 +1,31 @@
+package io.mrizzi;
+
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.math.BigDecimal;
+
+@Path("item")
+public class ItemResource {
+
+    @CheckedTemplate
+    public static class Templates {
+        public static native TemplateInstance item(Item item);
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get(@PathParam("id") Integer id) {
+//        return Templates.item(service.findItem(id));
+        Item item = new Item();
+        item.name = id.toString();
+        item.price = BigDecimal.valueOf(Math.random());
+        return Templates.item(item);
+    }
+}
